09a0db62592a75bbda183bd673975849fbaf3f9e
Carlos M. Martinez <carlos@xt6.us>
2018-11-05 08:25:57 +0000

    commit

--- docker-router-frr-rpki/start_frr_rpki.sh
+++ docker-router-frr-rpki/start_frr_rpki.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-/usr/lib/frr/zebra -d -f /etc/frr/zebra.conf
-/usr/lib/frr/bgpd -d -M rpki  -f /etc/frr/bgpd.conf
+#/usr/lib/frr/zebra -d -f /etc/frr/zebra.conf
+#/usr/lib/frr/bgpd -d -M rpki  -f /etc/frr/bgpd.conf
 #/usr/sbin/ospfd -d -f /etc/quagga/ospfd.conf
 #/usr/sbin/ospf6d -d -f /etc/quagga/ospf6d.conf
 #/usr/sbin/ripd -d -f /etc/quagga/ripd.conf
@@ -9,9 +9,16 @@
 #/usr/sbin/babeld -d -f /etc/quagga/babeld.conf
 #/usr/sbin/isisd -d -f /etc/quagga/isisd.conf
 
+/etc/init.d/frr start
+
 while true 
 do
 	echo "Entering FRR shell"
 	vtysh
+	for x in $(seq 1 3)
+	do
+		echo -n .
+		sleep 1
+	done
 done
 #/bin/bash
